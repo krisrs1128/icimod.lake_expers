040e7fb31a90260344a59cb4fbbdb99fc8cc7458
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-11 15:23:22 -0500

    make sure have access to data and results folders

--- cluster/landsat_test.sh
+++ cluster/landsat_test.sh
@@ -4,5 +4,4 @@ mv le7-2015-small.tar.gz data/
 tar -zxvf data/le7-small.tar.gz
 tar -zxvf icimod.glacial-lakes-baselines.tar.gz
 
-cd icimod.glacial-lakes-baselines
-python train.py --delse_pth MS_DeepLab_resnet_trained_VOC.pth --data_dir data/ --save_dir results/save --backup_dir results/backup
+python icimod.glacial-lakes-baselines/train.py --delse_pth MS_DeepLab_resnet_trained_VOC.pth --data_dir data/ --save_dir results/save --backup_dir results/backup
