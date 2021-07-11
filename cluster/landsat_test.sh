8eb90dc6050cba5cfd31199f84e036761889d2d0
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-11 13:33:45 -0500

    get access to the repository with all the models

--- cluster/landsat_test.sh
+++ cluster/landsat_test.sh
@@ -1,6 +1,8 @@
-
 mkdir results data
+
 mv le7-2015-small.tar.gz data/
 tar -zxvf data/le7-small.tar.gz
+tar -zxvf icimod.glacial-lakes-baselines.tar.gz
 
+cd icimod.glacial-lakes-baselines
 python train.py --delse_pth MS_DeepLab_resnet_trained_VOC.pth --data_dir data/ --save_dir results/save --backup_dir results/backup
