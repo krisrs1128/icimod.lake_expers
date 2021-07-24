e28c989903892b0d950d2d85cff18655c990e4a6
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-24 12:11:17 -0500

    Copy data from staging, instead of download from azure

--- cluster/landsat_test.sh
+++ cluster/landsat_test.sh
@@ -3,13 +3,30 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksankaran/miniconda3/envs/lakes/li
 eval "$(conda shell.bash hook)"
 conda activate /home/ksankaran/miniconda3/envs/lakes
 
+# copy data over from staging
+cp /staging/ksankaran/lakes/le7-2015.tar.gz . # change to bing_processed_data.tar.gz for bing
+cp /staging/ksankaran/lakes/MS_DeepLab_resnet_trained_VOC.pth .
+
 # unzip transferred data
 mkdir results data
-tar -zxvf le7-2015-small.tar.gz
+tar -zxvf le7-2015.tar.gz
 mv le7-2015 data/
 tar -zxvf icimod.glacial-lakes-baselines.tar.gz
 
-# start training
-python icimod.glacial-lakes-baselines/train.py --delse_pth MS_DeepLab_resnet_trained_VOC.pth --data_dir data/ --save_dir results/save --backup_dir results/backup --batch_size 1
+# clear unzipped data
+rm le7-2015.tar.gz
+rm icimod.glacial-lakes-baselines.tar.gz
 
+# start training
+python icimod.glacial-lakes-baselines/train.py \
+  --loss wbce \ # change to delse for delse model
+  --data_dir data/ \
+  --dataset landsat \ # change to "bing" for bing data
+  --delse_pth MS_DeepLab_resnet_trained_VOC.pth \
+  --save_dir results/save \
+  --backup_dir results/backup \
+  --batch_size 4 \
+  --n_epochs 20
+
+rm MS_DeepLab_resnet_trained_VOC.pth
 tar -zcvf results.tar.gz results/
