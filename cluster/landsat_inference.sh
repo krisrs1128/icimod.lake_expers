96558c1a3ea18b7558f2cfe3806b863b3e3e5180
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-24 10:52:26 -0500

    Add test scripts for inference

---
+++ cluster/landsat_inference.sh
@@ -0,0 +1,25 @@
+# setup environment
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksankaran/miniconda3/envs/lakes/lib
+eval "$(conda shell.bash hook)"
+conda activate /home/ksankaran/miniconda3/envs/lakes
+
+# copy data over from staging
+cp /staging/ksankaran/le7-2015.tar.gz .
+cp /staging/ksankaran/unet-landsat7-2.tar.gz .
+
+# unzip data and models
+mkdir results data
+tar -zxvf le7-2015.tar.gz
+tar -zxvf unet-landsat7-2.tar.gz
+tar -zxvf icimod.glacial-lakes-baselines.tar.gz
+mv le7-2015 data/
+
+# perform inference
+python icimod.glacial-lakes-baselines/inference.py \
+  --x_dir data/le7-2015/splits/val/images \
+  --meta_dir data/le7-2015/splits/val/meta \
+  --save_dir results/landsat-val \
+  --model_pth $(pwd)/unet-landsat7-2/bing_test_best.pth \
+  --inference_dir $(pwd)/results/landsat-val/
+
+tar -zcvf results.tar.gz results/
