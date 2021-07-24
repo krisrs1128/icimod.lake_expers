34ec2dd8e16a170c2bab633ecf28ba2318d222f3
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-24 12:11:47 -0500

    No need to refer to $(pwd) in inference

--- cluster/landsat_inference.sh
+++ cluster/landsat_inference.sh
@@ -14,12 +14,17 @@ tar -zxvf unet-landsat7-2.tar.gz
 tar -zxvf icimod.glacial-lakes-baselines.tar.gz
 mv le7-2015 data/
 
+# clear up zipped files
+rm le7-2015.tar.gz
+rm unet-landsat7-2.tar.gz
+rm icimod.glacial-lakes-baselines.tar.gz
+
 # perform inference
 python icimod.glacial-lakes-baselines/inference.py \
   --x_dir data/le7-2015/splits/val/images \
   --meta_dir data/le7-2015/splits/val/meta \
   --save_dir results/landsat-val \
-  --model_pth $(pwd)/unet-landsat7-2/bing_test_best.pth \
-  --inference_dir $(pwd)/results/landsat-val/
+  --model_pth unet-landsat7-2/bing_test_best.pth \
+  --inference_dir results/landsat-val/
 
 tar -zcvf results.tar.gz results/
