dc86b702902eaa8c93e86bb40366233a8e44a1bd
Kris Sankaran <sankaran.kris@gmail.com>
2021-08-24 15:06:10 -0500

    bing unet paths for azure

--- azure/bing_unet_inference.sh
+++ azure/bing_unet_inference.sh
@@ -3,38 +3,32 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksankaran/miniconda3/envs/lakes/li
 export MPLCONFIGDIR=$(pwd)
 export SKIMAGE_DATADIR=$(pwd)
 eval "$(conda shell.bash hook)"
-conda activate /home/ksankaran/miniconda3/envs/lakes
+conda activate /anaconda/envs/lakes
 
-# copy data over from staging
-mkdir results data
-cp /staging/ksankaran/lakes/labels/GL_3basins_2015* data/
-cp /staging/ksankaran/lakes/bing_glaciers_processed_small.tar.gz data/
-cp /staging/ksankaran/lakes/MS_DeepLab_resnet_trained_VOC.pth .
-cp /staging/wtian24/lakes/trained_models/bing-unet_best.pth data/
-
-# unzip data and models
-tar -zxvf icimod.glacial-lakes-baselines.tar.gz
-cd data
-tar -zxvf bing_glaciers_processed_small.tar.gz
-mv bing_glaciers_processed bing
-cd ..
-
-# perform inference
+# perform inference and evaluation on test set
 python icimod.glacial-lakes-baselines/inference.py \
-  --data_dir data/bing/splits/val \
+  --data_dir /datadrive/glaciers/bing_glaciers/processed/data/bing/splits/test \
   --x_dir images \
   --meta_dir meta \
   --stats_fn statistics.csv \
-  --model_pth data/bing-unet_best.pth \
-  --inference_dir results/bing_val-unet/ \
-  --dataset bing \
-  --input_channels 3\
-  --delse_pth MS_DeepLab_resnet_trained_VOC.pth
+  --model_pth /datadrive/results/backup/bing-unet_best.pth \
+  --inference_dir results/bing_test-unet/
 
 python icimod.glacial-lakes-baselines/evaluate.py \
-  --inference_dir results/bing_val-unet \
-  --save_dir results/bing_val-unet \
+  --inference_dir results/bing_test-unet \
+  --save_dir results/bing_test-unet \
   --vector_label data/GL_3basins_2015.shp
 
-rm icimod.glacial-lakes-baselines.tar.gz MS_DeepLab_resnet_trained_VOC.pth
-tar -zcvf bing_unet_inference.tar.gz results/
+# inference and evaluation overall
+python icimod.glacial-lakes-baselines/inference.py \
+  --data_dir /datadrive/glaciers/bing_glaciers/processed/data/bing/ \
+  --x_dir images \
+  --meta_dir meta \
+  --stats_fn statistics.csv \
+  --model_pth /datadrive/results/backup/bing-unet_best.pth \
+  --inference_dir results/bing-unet/
+
+python icimod.glacial-lakes-baselines/evaluate.py \
+  --inference_dir results/bing-unet \
+  --save_dir results/bing-unet \
+  --vector_label data/GL_3basins_2015.shp
--- cluster/bing_unet_inference.sh
+++ cluster/bing_unet_inference.sh
@@ -8,32 +8,42 @@ conda activate /home/ksankaran/miniconda3/envs/lakes
 # copy data over from staging
 mkdir results data
 cp /staging/ksankaran/lakes/labels/GL_3basins_2015* data/
-cp /staging/ksankaran/lakes/bing_glaciers_processed_small.tar.gz data/
+cp /staging/ksankaran/lakes/bing_glaciers_processed.tar.gz data/
 cp /staging/ksankaran/lakes/MS_DeepLab_resnet_trained_VOC.pth .
 cp /staging/wtian24/lakes/trained_models/bing-unet_best.pth data/
 
 # unzip data and models
 tar -zxvf icimod.glacial-lakes-baselines.tar.gz
 cd data
-tar -zxvf bing_glaciers_processed_small.tar.gz
-mv bing_glaciers_processed bing
+tar -zxvf bing_glaciers_processed.tar.gz
 cd ..
 
-# perform inference
+# perform inference and evaluation on test set
 python icimod.glacial-lakes-baselines/inference.py \
-  --data_dir data/bing/splits/val \
+  --data_dir data/bing/splits/test \
   --x_dir images \
   --meta_dir meta \
   --stats_fn statistics.csv \
   --model_pth data/bing-unet_best.pth \
-  --inference_dir results/bing_val-unet/ \
-  --dataset bing \
-  --input_channels 3\
-  --delse_pth MS_DeepLab_resnet_trained_VOC.pth
+  --inference_dir results/bing_test-unet/
 
 python icimod.glacial-lakes-baselines/evaluate.py \
-  --inference_dir results/bing_val-unet \
-  --save_dir results/bing_val-unet \
+  --inference_dir results/bing_test-unet \
+  --save_dir results/bing_test-unet \
+  --vector_label data/GL_3basins_2015.shp
+
+# inference and evaluation overall
+python icimod.glacial-lakes-baselines/inference.py \
+  --data_dir data/bing/ \
+  --x_dir images \
+  --meta_dir meta \
+  --stats_fn statistics.csv \
+  --model_pth data/bing-unet_best.pth \
+  --inference_dir results/bing-unet/
+
+python icimod.glacial-lakes-baselines/evaluate.py \
+  --inference_dir results/bing-unet \
+  --save_dir results/bing-unet \
   --vector_label data/GL_3basins_2015.shp
 
 rm icimod.glacial-lakes-baselines.tar.gz MS_DeepLab_resnet_trained_VOC.pth
