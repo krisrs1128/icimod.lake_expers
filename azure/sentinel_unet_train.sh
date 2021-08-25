8fa38ea1902c11974268d7639e212afc762d0680
Kris Sankaran <sankaran.kris@gmail.com>
2021-08-25 10:50:20 -0500

    sentinel training on azure

--- azure/sentinel_unet_train.sh
+++ azure/sentinel_unet_train.sh
@@ -3,34 +3,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ksankaran/miniconda3/envs/lakes/li
 export MPLCONFIGDIR=$(pwd)
 export SKIMAGE_DATADIR=$(pwd)
 eval "$(conda shell.bash hook)"
-conda activate /home/ksankaran/miniconda3/envs/lakes
-
-# copy data over from staging
-cp /staging/ksankaran/lakes/sentinel.tar.gz .
-cp /staging/ksankaran/lakes/MS_DeepLab_resnet_trained_VOC.pth .
-
-# unzip transferred data
-mkdir results data
-tar -zxvf sentinel.tar.gz
-mv sentinel data/
-tar -zxvf icimod.glacial-lakes-baselines.tar.gz
-
-# clear unzipped data
-rm sentinel.tar.gz
-rm icimod.glacial-lakes-baselines.tar.gz
+conda activate /anaconda/envs/lakes
 
 # start training
 python icimod.glacial-lakes-baselines/train.py \
   --experiment_name sentinel-unet \
-  --data_dir data/ \
+  --data_dir /datadrive/snake/lakes/sentinel/ \
   --dataset sentinel \
-  --delse_pth MS_DeepLab_resnet_trained_VOC.pth \
-  --save_dir results/save \
-  --backup_dir results/backup \
-  --log_dir results/logs \
+  --delse_pth /datadrive/snake/models/MS_DeepLab_resnet_trained_VOC.pth \
+  --save_dir /datadrive/results/save \
+  --backup_dir /datadrive/results/backup \
+  --log_dir /datadrive/results/logs \
   --batch_size 8 \
   --divergence \
   --n_epochs 20
-
-rm MS_DeepLab_resnet_trained_VOC.pth
-tar -zcvf sentinel_unet_trained.tar.gz results/
