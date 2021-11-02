d4e15385fe88c6d14e60b97b1ea687da04e258c9
Kris Sankaran <sankaran.kris@gmail.com>
2021-11-02 15:04:46 +0000

    automatically compute metrics at best IoU

--- azure/metrics.R
+++ azure/metrics.R
@@ -2,14 +2,15 @@
 library(dplyr)
 library(purrr)
 
-metrics_dir <- "/home/kris/"
-paths <- list.files(metrics_dir, "*.csv", full = TRUE)
+metrics_dir <- "/datadrive/results/inference/compressed/"
+paths <- list.files(metrics_dir, "*.csv", full = TRUE, recursive = TRUE)
 metrics <- map_dfr(paths, read.csv, .id = "path") %>%
-	mutate(model = basename(paths[as.integer(path)]))
+	mutate(model = gsub(metrics_dir, "", paths[as.integer(path)]))
 
 avg_metrics <- metrics %>%
-	group_by(model) %>%
+	group_by(model, prob) %>%
 	summarize_at(vars(IoU, precision, recall, frechet), mean) %>%
+	filter(IoU == max(IoU)) %>%
 	mutate_at(vars(IoU, precision, recall, frechet), ~ round(., 4))
 
 write.csv(metrics, "metrics_combined.csv", row.names = FALSE)
